ae2e4c7113e17e0bc2b195c9cf93855d889c5af4
StalckGx <vinny.polsom@outlook.com>
2021-03-16 02:27:56 +0044

    Fully playable - Replay box functioning - give id to selected boxes - changed variable names

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
-const selectBox = document.querySelector(".select-box"),
+const
+	selectBox = document.querySelector(".select-box"),
 	selectXButton = selectBox.querySelector(".playerX"),
 	selectOButton = selectBox.querySelector(".playerO"),
 	playboard = document.querySelector(".playboard"),
 	allBoxes = document.querySelectorAll("section span"),
-	players = document.querySelector(".players");
+	players = document.querySelector(".players"),
+	resultBox = document.querySelector(".result-box"),
+	winnerText = resultBox.querySelector(".winner-text"),
+	replayButton = resultBox.querySelector(".replay-btn button");
 
 // Quando a tela carrega
 window.onload = () => {
@@ -27,27 +31,37 @@ window.onload = () => {
 }
 
 // FontAwesome symbols
-let playerXSymbol = "fas fa-times";
-let playerOSymbol = "far fa-circle";
+let xSymbol = "fas fa-times";
+let oSymbol = "far fa-circle";
+let playerSymbol;
+let gameOver = false;
 
 // Quando uma caixa é clickada pelo usuário
 function clickedBox(element) {
 	if (players.classList.contains("player")) {
+		playerSymbol = "O";
 		// Addiciona o simbolo
-		element.innerHTML = `<i class="${playerOSymbol}"></i>`;
+		element.innerHTML = `<i class="${oSymbol}"></i>`;
 	} else {
-		element.innerHTML = `<i class="${playerXSymbol}"></i>`;
+		playerSymbol = "X";
+		element.innerHTML = `<i class="${xSymbol}"></i>`;
 	}
 	// Muda de jogador
 	players.classList.add("active");
-	// Faz o element não ser mais clickável
+	// Concede 
+	element.setAttribute("id", playerSymbol)
+	// Faz a caixa não ser mais clickável
+	playboard.style.pointerEvents = "none";
 	element.style.pointerEvents = "none";
 
+	checkForWinner();
 
-	let randomDelayTime = ((Math.random() * 1000) + 200).toFixed();
-	setTimeout(() => {
-		bot();
-	}, randomDelayTime);
+	if (!gameOver) {
+		let randomDelayTime = ((Math.random() * 1000) + 200).toFixed();
+		setTimeout(() => {
+			bot();
+		}, randomDelayTime);
+	}
 }
 
 function bot(){
@@ -55,18 +69,74 @@ function bot(){
 	for (let i = 0; i < allBoxes.length; i++) {
 		if (allBoxes[i].childElementCount == 0) {
 			array.push(i);
-			console.log(i+" has no children");
 		}
 	}
-	let randomBox = array[Math.floor(Math.random() * array.length)];
 	if (array.length > 0) {
+		let randomBox = array[Math.floor(Math.random() * array.length)];
 		if (players.classList.contains("player")) {
-			// Addiciona o simbolo
-			allBoxes[randomBox].innerHTML = `<i class="${playerXSymbol}"></i>`;
+			playerSymbol = "X";
+			allBoxes[randomBox].innerHTML = `<i class="${xSymbol}"></i>`;
 		} else {
-			allBoxes[randomBox].innerHTML = `<i class="${playerOSymbol}"></i>`;
+			playerSymbol = "O";
+			allBoxes[randomBox].innerHTML = `<i class="${oSymbol}"></i>`;
 		}
 		players.classList.remove("active");
+		allBoxes[randomBox].setAttribute("id", playerSymbol)
+		allBoxes[randomBox].style.pointerEvents = "none";
+		playboard.style.pointerEvents = "auto";
 	}
-	allBoxes[randomBox].style.pointerEvents = "none";
+	checkForWinner();
+}
+
+function getBoxId(idName) {
+	return document.querySelector(".box" + idName).id;
+}
+function checkForWinner() {
+	if (isMatch() || isAllBoxesMarked()) {
+		gameOver = true;
+		// Fecha a caixa de jogo e abre a de vencedor
+		setTimeout(() => {
+			playboard.classList.remove("show");
+			resultBox.classList.add("show");
+		}, 700);
+		if (!isMatch()) {
+			winnerText.innerHTML = `Deu velha!`;
+		} else {
+			winnerText.innerHTML = `O jogador <p>${playerSymbol}</p> venceu!`;
+		}
+	}
+}
+
+// Verifica se hà uma combinação
+function isMatch() {
+	if (checkMatch(1,2,3,playerSymbol) || checkMatch(4,5,6, playerSymbol) || checkMatch(7,8,9, playerSymbol) || checkMatch(1,4,7, playerSymbol) || checkMatch(2,5,8, playerSymbol) || checkMatch(3,6,9, playerSymbol) || checkMatch(1,5,9, playerSymbol) || checkMatch(3,5,7, playerSymbol)) {
+		return true;
+	} else {
+		return false;
+	}
+}
+function checkMatch(val1, val2, val3, symbol) {
+	if (getBoxId(val1) == symbol && getBoxId(val2) == symbol && getBoxId(val3) == symbol) {
+		return true;
+	}
+}
+
+// Verifica se todas as caixas foram marcadas
+function isAllBoxesMarked() {
+	let emptyBoxExists = false;
+	for (let i = 1; i < 10; i++) {
+		if(getBoxId(i) == "") {
+			emptyBoxExists = true;
+			break;
+		}
+	}
+	if (emptyBoxExists) {
+		return false;
+	} else {
+		return true;
+	}
+}
+
+replayButton.onclick = () => {
+	window.location.reload();
 }
